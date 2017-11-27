a4e38f72467b8b9841ad66a18592939a60911ed1
shadowfax <aagamshah@public-docking-als-0128.ethz.ch>
2017-11-27 19:23:58 +0100

    TestCommit2

--- LFPutils/evoked_lfp_analysis.py
+++ LFPutils/evoked_lfp_analysis.py
@@ -37,8 +37,8 @@ for folder in (folder for folder in dirs if ((folder != 'log.txt') and (folder !
 	p = pickle.load(open(mainPath + folder + '/paramsDict.p', 'rb'))
 	if (p['probes'] == 2) and (a == 0):
 	#Creating multiple sheets in the excel output if there are multiple probes
-        writer_1 = ExcelWriter(analyzed_path + 'peak_data_probe_1.xlsx', engine = 'xlsxwriter')
-    	a = a+1
+		writer_1 = ExcelWriter(analyzed_path + 'peak_data_probe_1.xlsx', engine = 'xlsxwriter')
+		a = a+1
 	peak_info = {key: {} for key in range(p['probes'])}
 
 	#Make folder for the analysis results of the recording session
@@ -47,60 +47,60 @@ for folder in (folder for folder in dirs if ((folder != 'log.txt') and (folder !
 		os.mkdir(analyzed_path_for_folder)
 
 	for probe in range(p['probes']):
-    	peak_info[probe]['peak_locs'], peak_info[probe]['peak_stds'], peak_info[probe]['peak_times'], peak_info[probe]['peak_errs'], peak_info[probe]['peak_amps'] = (np.zeros(p['nr_of_electrodes']) for i in range(5))
-    	for shank in range(p['shanks']):
+		peak_info[probe]['peak_locs'], peak_info[probe]['peak_stds'], peak_info[probe]['peak_times'], peak_info[probe]['peak_errs'], peak_info[probe]['peak_amps'] = (np.zeros(p['nr_of_electrodes']) for i in range(5))
+		for shank in range(p['shanks']):
         	#Load the data for the shank
-            data = pickle.load(open(mainPath + folder + '/probe_{:g}_shank_{:g}/probe_{:g}_shank_{:g}_evoked.pickle'.format(probe,shank,probe,shank), 'rb'))
+			data = pickle.load(open(mainPath + folder + '/probe_{:g}_shank_{:g}/probe_{:g}_shank_{:g}_evoked.pickle'.format(probe,shank,probe,shank), 'rb'))
 
             #Make folder for the analysis results of the shank
-            analyzed_path_for_shank = analyzed_path_for_folder + '/probe_{:g}_shank_{:g}'.format(probe,shank)
-            if not os.path.exists(analyzed_path_for_shank):
-                os.mkdir(analyzed_path_for_shank)
+			analyzed_path_for_shank = analyzed_path_for_folder + '/probe_{:g}_shank_{:g}'.format(probe,shank)
+			if not os.path.exists(analyzed_path_for_shank):
+				os.mkdir(analyzed_path_for_shank)
 
-            evoked = data['evoked'] #Evoked LFP waveforms
-            evoked_avg = np.mean(evoked,0) #Average evoked LFP waveforms across trials
-            evoked_std = np.std(evoked, 0) #Standard deviation of the evoked LFP waveforms across trials
-            evoked_err = evoked_std / math.sqrt(len(evoked)) #Standard error of the evoked LFP waveforms across trials
-            peak_info[probe]['peak_amps'][shank*p['nr_of_electrodes_per_shank']:(shank+1)*p['nr_of_electrodes_per_shank']] = np.min(evoked_avg, 1)
-            time = np.linspace(-p['evoked_pre']*1000, p['evoked_post']*1000, (p['evoked_post'] + p['evoked_pre']) * p['sample_rate'])
+			evoked = data['evoked'] #Evoked LFP waveforms
+			evoked_avg = np.mean(evoked,0) #Average evoked LFP waveforms across trials
+			evoked_std = np.std(evoked, 0) #Standard deviation of the evoked LFP waveforms across trials
+			evoked_err = evoked_std / math.sqrt(len(evoked)) #Standard error of the evoked LFP waveforms across trials
+			peak_info[probe]['peak_amps'][shank*p['nr_of_electrodes_per_shank']:(shank+1)*p['nr_of_electrodes_per_shank']] = np.min(evoked_avg, 1)
+			time = np.linspace(-p['evoked_pre']*1000, p['evoked_post']*1000, (p['evoked_post'] + p['evoked_pre']) * p['sample_rate'])
 
 			#Make a plot for the electrodes on the shank
-            figure()
-            y = np.linspace(0,800,8)
-            pcolor(time,y,evoked_avg)
-            colorbar()
-            xlabel('Time(ms)')
-            ylabel('Height from tip (um)')
-            savefig(analyzed_path_for_shank+'/probe_{:g}_shank_{:g}_evoked.svg'.format(probe,shank), format = 'svg')
-            close()
+			figure()
+			y = np.linspace(0,800,8)
+			pcolor(time,y,evoked_avg)
+			colorbar()
+			xlabel('Time(ms)')
+			ylabel('Height from tip (um)')
+			savefig(analyzed_path_for_shank+'/probe_{:g}_shank_{:g}_evoked.svg'.format(probe,shank), format = 'svg')
+			close()
 
 			#Calculate the peak parameters for each electrode and plot the average evoked LFP waveform for each electrode
-            for trode in range(p['nr_of_electrodes_per_shank']):
-                real_trode = p['nr_of_electrodes_per_shank']*shank + trode
-                try:
-                    peak_info[probe]['peak_locs'][real_trode] = np.where(evoked_avg[trode] == peak_info[probe]['peak_amps'][real_trode])[0]
-                    peak_info[probe]['peak_stds'][real_trode] = evoked_std[trode][int(peak_info[probe]['peak_locs'][real_trode])]
-                    peak_info[probe]['peak_errs'][real_trode] = evoked_err[trode][int(peak_info[probe]['peak_locs'][real_trode])]
-                    peak_info[probe]['peak_times'][real_trode] = (peak_info[probe]['peak_locs'][real_trode] - p['evoked_pre']) / p['sample_rate']
+			for trode in range(p['nr_of_electrodes_per_shank']):
+				real_trode = p['nr_of_electrodes_per_shank']*shank + trode
+				try:
+					peak_info[probe]['peak_locs'][real_trode] = np.where(evoked_avg[trode] == peak_info[probe]['peak_amps'][real_trode])[0]
+					peak_info[probe]['peak_stds'][real_trode] = evoked_std[trode][int(peak_info[probe]['peak_locs'][real_trode])]
+					peak_info[probe]['peak_errs'][real_trode] = evoked_err[trode][int(peak_info[probe]['peak_locs'][real_trode])]
+					peak_info[probe]['peak_times'][real_trode] = (peak_info[probe]['peak_locs'][real_trode] - p['evoked_pre']) / p['sample_rate']
 
-                except ValueError:
-                    pass
+				except ValueError:
+					pass
 
-                figure()
-                plot(time, evoked_avg[trode], 'k-')
-                fill_between(time, evoked_avg[trode]-evoked_err[trode], evoked_avg[trode]+evoked_err[trode])
-                xlabel('Time (ms)')
-                ylabel('Voltage (uV)')
-                savefig(analyzed_path_for_shank + '/electrode' + str(trode) + '_evoked.svg', format = 'svg')
-                close()
+				figure()
+				plot(time, evoked_avg[trode], 'k-')
+				fill_between(time, evoked_avg[trode]-evoked_err[trode], evoked_avg[trode]+evoked_err[trode])
+				xlabel('Time (ms)')
+				ylabel('Voltage (uV)')
+				savefig(analyzed_path_for_shank + '/electrode' + str(trode) + '_evoked.svg', format = 'svg')
+				close()
 
     #Saving the peak parameters for each recording session into an excel file
-    df0 = DataFrame({'Peak amplitudes': peak_info[0]['peak_amps'], 'Peak std': peak_info[0]['peak_stds'], 'Peak standard errors': peak_info[0]['peak_errs'], 'Peak times': peak_info[0]['peak_times']})
-    df0.to_excel(writer_0, sheet_name = folder[0:30])
-    if p['probes'] == 2:
-        df1 = DataFrame({'Peak amplitudes': peak_info[1]['peak_amps'], 'Peak std': peak_info[1]['peak_stds'], 'Peak standard errors': peak_info[1]['peak_errs'], 'Peak times': peak_info[1]['peak_times']})
-        df1.to_excel(writer_1, sheet_name = folder[0:30])
+	df0 = DataFrame({'Peak amplitudes': peak_info[0]['peak_amps'], 'Peak std': peak_info[0]['peak_stds'], 'Peak standard errors': peak_info[0]['peak_errs'], 'Peak times': peak_info[0]['peak_times']})
+	df0.to_excel(writer_0, sheet_name = folder[0:30])
+	if p['probes'] == 2:
+		df1 = DataFrame({'Peak amplitudes': peak_info[1]['peak_amps'], 'Peak std': peak_info[1]['peak_stds'], 'Peak standard errors': peak_info[1]['peak_errs'], 'Peak times': peak_info[1]['peak_times']})
+		df1.to_excel(writer_1, sheet_name = folder[0:30])
 
 writer_0.save()
 if p['probes'] == 2:
-    writer_1.save()
+	writer_1.save()
